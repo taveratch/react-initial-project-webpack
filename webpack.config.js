8886875200610995072a52c433742f1fa0563749
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-12-06 11:02:17 +0700

    fix slow build

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,6 @@ module.exports = {
       'process.env': {
         NODE_ENV: JSON.stringify('production')
       }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
     })
   ]
 };
